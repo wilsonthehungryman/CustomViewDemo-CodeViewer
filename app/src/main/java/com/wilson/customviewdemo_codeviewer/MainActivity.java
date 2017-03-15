e98ecf53ee6e208239858999a866e2448e05d0af
Wilson.ST <wilsonthehungryman@gmail.com>
2017-03-14 19:38:38 -0500

    Some configurations for displayed text/source code

--- app/src/main/java/com/wilson/customviewdemo_codeviewer/MainActivity.java
+++ app/src/main/java/com/wilson/customviewdemo_codeviewer/MainActivity.java
@@ -5,9 +5,36 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CodeViewer codeViewer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        codeViewer = (CodeViewer)findViewById(R.id.codeViewer);
+        codeViewer.setSourceCode("@Override\n" +
+                "    protected void onDraw(Canvas canvas) {\n" +
+                "        super.onDraw(canvas);\n" +
+                "\n" +
+                "        // TODO: consider storing these as member variables to reduce\n" +
+                "        // allocations per draw cycle.\n" +
+                "        mPaddingLeft = getPaddingLeft();\n" +
+                "          mPaddingTop = getPaddingTop();\n" +
+                "         mPaddingRight = getPaddingRight();\n" +
+                "        mPaddingBottom = getPaddingBottom();\n" +
+                "        mContentWidth = getWidth() - mPaddingLeft - mPaddingRight;\n" +
+                "        mContentHeight = getHeight() - mPaddingTop - mPaddingBottom;\n" +
+                "\n" +
+                "        // Draw the text.\n" +
+                "        canvas.drawText(rawSourceCode,\n" +
+                "                mPaddingLeft + (mContentWidth) / 2,\n" +
+                "                mPaddingTop + (mContentHeight) / 2,\n" +
+                "                mDefaultPaint);\n" +
+                "    }");
     }
 }
