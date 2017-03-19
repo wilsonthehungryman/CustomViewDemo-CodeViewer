4550201685a8566c93ed7b105da5c664e28b0728
Wilson.ST <wilsonthehungryman@gmail.com>
2017-03-19 14:12:48 -0500

    Swapped out code sample

--- app/src/main/java/com/wilson/customviewdemo_codeviewer/MainActivity.java
+++ app/src/main/java/com/wilson/customviewdemo_codeviewer/MainActivity.java
@@ -17,38 +17,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         codeViewer = (CodeViewer)findViewById(R.id.codeViewer);
-        codeViewer.setSourceCode("private int mPaddingLeft, mPaddingTop, mPaddingRight, mPaddingBottom, mContentWidth, mContentHeight;\n" +
-                "private int mKeywordColor, mVariableColor, mPrimitiveColor, mLiteralColor, mCommentColor;\n" +
-                "    private int mLanguage, mTabLength;\n" +
-                "    private boolean mWrapLines;\n" +
-                "    private String rawSourceCode, mTab;\n" +
-                "    private Paint mDefaultPaint, mKeyWordPaint, mCommentPaint, mLiteralPaint;\n" +
-                "    private StringBuilder mStringBuilder;\n" +
-                "    private Pattern mBlockBeginSign, mBlockEndSign, mKeyWords, mComment, mNumeric;\n" +
-                "    private Matcher matcher;\n" +
-                "@Override\n" +
-                "    protected void onDraw(Canvas canvas) {\n" +
-                "        super.onDraw(canvas);\n" +
+        codeViewer.setSourceCode("public class sample extends Object {\n" +
+                "  private int integer;\n" +
+                "  protected boolean booly;\n" +
+                "  private final double tax = 0.05;\n" +
+                "  private char c;\n" +
+                "  private String s;\n" +
                 "\n" +
-                "        // TODO: consider storing these as member variables to reduce\n" +
-                "        // allocations per draw cycle.\n" +
-                "        mPaddingLeft = getPaddingLeft();\n" +
-                "          mPaddingTop = getPaddingTop();\n" +
-                "         mPaddingRight = getPaddingRight();\n" +
-                "        mPaddingBottom = getPaddingBottom();\n" +
-                "        mContentWidth = getWidth() - mPaddingLeft - mPaddingRight;\n" +
-                "        mContentHeight = getHeight() - mPaddingTop - mPaddingBottom;\n" +
+                "  public sample(int integer, boolean mrBooly)\n" +
+                "  {\n" +
+                "    this.integer = integer;\n" +
+                "    this.booly = mrBooly;\n" +
                 "\n" +
-                "        // Draw the text.\n" +
-                "        canvas.drawText(rawSourceCode,\n" +
-                "                mPaddingLeft + (mContentWidth) / 2,\n" +
-                "                mPaddingTop + (mContentHeight) / 2,\n" +
-                "                mDefaultPaint);\n" +
-                "        mContentHeight = getHeight() - mPaddingTop - mPaddingBottom;\n" +
+                "  }\n" +
                 "\n" +
-                "        int final number = 19.19" +
-                "\n" +
-                "        String literal = \" something\"" +
-                "    }");
+                "  public boolean setOthers(){\n" +
+                "    c = ' ';\n" +
+                "    s = \"something\\\" something'\"\n" +
+                "    char otherchar = '\\\"';\n" +
+                "  }\n" +
+                "}");
     }
 }
